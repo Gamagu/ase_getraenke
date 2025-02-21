01b8a1b1bec437c936faafb47c33474a6be2cb39
Yannis Moser <87710293+Kronprinz03@users.noreply.github.com>
2025-02-21 16:21:29 +0100

    Futher development of GetraenkeInputHandler

--- asegetraenke/getraenkeadapter/src/main/java/com/asegetraenke/console/GetraenkeInputHandler.java
+++ asegetraenke/getraenkeadapter/src/main/java/com/asegetraenke/console/GetraenkeInputHandler.java
@@ -1,5 +1,6 @@
 package com.asegetraenke.console;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,12 +10,14 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-import com.example.entities.Produkt;
-import com.example.valueobjects.Preis;
-import com.example.getraenkeusecases;
+import com.asegetraenke.entities.Produkt;
+import com.asegetraenke.valueobjects.Pfandwert;
+import com.asegetraenke.valueobjects.Preis;
+import com.asegetraenke.getraenkeusecases;
 
 public class GetraenkeInputHandler {
     private final String NOPRODUKTMESSAGE = "There are no Product/s found";
+    private final String NOPFANDWERTMESSAGE = "There are no Pfandwert/s found";
 
     private final Scanner scanner;
     private final Map<String,Runnable> getrankeCommandMap;
@@ -50,28 +53,92 @@ public class GetraenkeInputHandler {
     public void handleAcceptLieferungInput() {
         // Handle acceptLieferung logic here
     }
-    //TODO Ask Nikals what he was thinking here 
+
     public void handleAddPfandWertInput() {
-    // Handle addPfandWert logic here
+        System.out.println("Add new Pfand value to existing");
+        String description = readStringInputWithPrompt("Description: ");
+        Double value = readDoubleInputWithPrompt("Value: ");
+        if(!acceptInput()){
+            return;
+        }
+        try {
+            getraenkeusecases.addPfandWert(description, value);
+        } catch (Exception e) {
+            System.out.println("An Error Occurt while storing the new Pfandwert: \n"+ 
+                                "Value: " + value +"\n"+
+                                "Description: "+ description);
+            System.err.println(e.getMessage());
+        }
     }
 
-    //TODO Ask Nikals what he is thinking here 
     public void handleSetPfandwertInput() {
-    // Handle setPfandwert logic here
-    }
-    //TODO Ask Nikals what he is thinking here 
-    public void handleSetPfandwertProduktInput() {
-    // Handle setPfandwertProdukt logic here
-    }
-    //TODO Ask Nikals what he is thinking here 
-    public void handleGetAllPfandwerteInput() {
-    // Handle getAllPfandwerte logic here
-    }
-    //TODO Ask Nikals what he is thinking here 
-    public void handleGetPfandWertInput() {
-    // Handle getPfandWert logic here
+        Optional<Pfandwert> pfandwerOptional = pickOnePfandwertFromAllPfandwerts();
+        if(pfandwerOptional.isEmpty()){
+            errorNoPfandWert();
+            return;
+        }
+        Pfandwert pfandwert = pfandwerOptional.get();
+        double newValue = readDoubleInputWithPrompt("New Value for Pfandwert: ");
+        if(!acceptInput()){
+            return;
+        }
+        try {
+            getraenkeusecases.setPfandwert(pfandwert, newValue);
+        } catch (Exception e) {
+            System.out.println("An Error Occurt while setting the new Pfandwert: \n"+ 
+            "Pfandwert: " + pfandwert.toString() +"\n"+
+            "New Value: "+ newValue);
+            System.err.println(e.getMessage());
+        }
     }
     
+    //TODO Ask Nikals what he is thinking here 
+    public void handleSetPfandwertProduktInput() {
+        Optional<Pfandwert> pfandwertOptional = pickOnePfandwertFromAllPfandwerts();
+        if (pfandwertOptional.isEmpty()) {
+            errorNoPfandWert();
+            return;
+        }
+        Pfandwert pfandwert = pfandwertOptional.get();
+
+        Optional<Produkt> produktOptional = pickOneProductFromAllProducts();
+        if(produktOptional.isEmpty()){
+            errorNoProdukt();
+            return;
+        }
+        Produkt produkt = produktOptional.get();
+        Iterable<Pfandwert> pfandwertVec = Collections.singleton(pfandwert);
+        try {
+            getraenkeusecases.setPfandwertProdukt(produkt, pfandwertVec);
+        } catch (Exception e) {
+            System.out.println("An Error Occurt while storing the : \n"+ 
+            "Pfandwert: " + pfandwert.toString() +"\n"+
+            "Product: "+ produkt.toString());
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void handleGetAllPfandwerteInput() {
+        Iterable<Pfandwert> pfandwertVec = getraenkeusecases.getAllPfandwerte();
+        List<Pfandwert> pfandwertList = StreamSupport.stream(pfandwertVec.spliterator(), false)
+                                    .collect(Collectors.toList());
+        if(pfandwertList.isEmpty()){
+            errorNoProdukt();
+            return;
+        }
+        int count = 1;
+        for(Pfandwert pfandwert : pfandwertList){
+            printPfandwertWithNumber(pfandwert, count);
+            count++;
+        }
+    }
+    
+    public void handleGetPfandWertInput() {
+        String uuidString = readStringInputWithPrompt("UUID of Pfandwert: ");
+        UUID uuid = UUID.fromString(uuidString);
+        Pfandwert pfandwert = getraenkeusecases.getPfandWert(uuid);
+        printPfandwertWithNumber(pfandwert, 1);
+    }
     
     public void handleGetAllProductsInput() {
         Iterable<Produkt> productVec = getraenkeusecases.getAllProducts();
@@ -212,8 +279,37 @@ public class GetraenkeInputHandler {
         return Optional.of(productList.get(indexProdukt-1));
     }
 
+    private Optional<Pfandwert> pickOnePfandwertFromAllPfandwerts() {
+        Iterable<Pfandwert> pfandwertVec = getraenkeusecases.getAllPfandwerte();
+        List<Pfandwert> pfandwertList = StreamSupport.stream(pfandwertVec.spliterator(), false)
+                                    .collect(Collectors.toList());
+        if(pfandwertList.isEmpty()){
+            return Optional.empty();
+        }
+
+        int count = 1;
+        for(Pfandwert pfandwert : pfandwertList) {
+            printPfandwertWithNumber(pfandwert, count);
+        }
+        int indexProdukt = 0;
+        while (true) {
+            indexProdukt = readIntInputWithPrompt("Which Customername do you want to change? Enter the Number: ");
+            if(indexProdukt < pfandwertList.size()+1 && indexProdukt > 0){
+                break;
+            }
+            System.out.println("Something went wrong the "+ indexProdukt +  " is not in the list");
+        }
+        Pfandwert pfandwert = pfandwertList.get(indexProdukt-1);
+        System.out.println("Chosen Produkt: "+ pfandwert.toString());
+        return Optional.of(pfandwertList.get(indexProdukt-1));
+    }
+
     private void printProduktWithNumber(Produkt produkt, int number){
         System.out.println(number + ". "+ produkt.toString());
+    }
+
+    private void printPfandwertWithNumber(Pfandwert pfandwert, int number){
+        System.out.println(number + ". "+ pfandwert.toString());
     }
 
     private int readIntInputWithPrompt(String prompt){
@@ -222,6 +318,19 @@ public class GetraenkeInputHandler {
             String input = this.scanner.nextLine();
             try{
                 int inputCastInt = Integer.parseInt(input);
+                return inputCastInt;
+            }catch(Exception e){
+                System.out.println("The input: "+ input+ " can not be translated into a number");
+            }
+        }
+    }
+
+    private Double readDoubleInputWithPrompt(String prompt){
+        System.out.print(prompt);
+        while(true){
+            String input = this.scanner.nextLine();
+            try{
+                double inputCastInt = Double.parseDouble(input);
                 return inputCastInt;
             }catch(Exception e){
                 System.out.println("The input: "+ input+ " can not be translated into a number");
@@ -253,5 +362,8 @@ public class GetraenkeInputHandler {
         }
     }
 
+    private void errorNoPfandWert() {
+        System.out.println(NOPFANDWERTMESSAGE);
+    }
     
 }
